70bd369936f006a1cba53f89e893f17c36a62056
Mason C <43713560+mercurrie@users.noreply.github.com>
2022-12-15 22:52:14 -0600

    refactor code
    turned while loop that handles the user's guesses into function user_guesses()
    
    included type cast to functions
    
    changed if statement in level_of_difficulty(() into Dict

--- main.py
+++ main.py
@@ -2,24 +2,12 @@ import random
 from art import logo
 
 # level_of_difficulty() returns number of attempts based on difficulty selection
-def level_of_difficulty(choice):
-    if choice == 'easy':
-        return 10
-    else:
-        return 5
-    
-# play() generates random number, takes im input from user, and compared that input to the number generated 
-def play():
-    print(logo)
-    print('Welcome to the number guessing game!')
-    print("I'm thinking of a number between 1 and 100.")
-    answer = random.randint(1,100)
-    print(f'pssst the number is {answer}')
-    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    
-    attempts = level_of_difficulty(choice = difficulty)
+def level_of_difficulty(choice: str)-> str:
+    difficulty = {'easy': 10, 'hard': 5}
+    return difficulty[choice]
+
+def user_guesses(attempts: int, answer: int):
     guess = 0
-    
     while attempts > 0 and guess != answer:
         print(f'\nYou have {attempts} attempts remaining to guess the number. ')
         guess = int(input('Pick a number. '))
@@ -28,13 +16,24 @@ def play():
         elif guess < answer:
             print('Your guess is too low. \nGuess again.')
         attempts -= 1
-    
-    if guess == answer:
-        print('You guessed the number. You win!')
-    else:
-        print('You ran out of attempts. You lose.')
         
+        if guess == answer:
+            print('You guessed the number. You win!')
+        else:
+            print('You ran out of attempts. You lose.')
+            
+# play() generates random number, takes im input from user, and compared that input to the number generated 
+def play():
+    print(logo)
+    print('Welcome to the number guessing game!')
+    print("I'm thinking of a number between 1 and 100.")
+    solution = random.randint(1,100)
+    print(f'pssst the number is {solution}')
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
     
+    number_of_guesses = level_of_difficulty(choice = difficulty)
+    
+    user_guesses(attempts = number_of_guesses , answer = solution)
     
 # used to call play()
 if __name__ == '__main__':
